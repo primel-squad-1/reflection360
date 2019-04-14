e9feb0ce121fc2e258c16474ac696be7e9e2e098
Ishy-chawla <ishkaur9@gmail.com>
2019-04-14 19:52:04 +1000

    Add files via upload

---
+++ client_signup.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2019 at 11:48 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `candidate page`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_signup`
+--
+
+CREATE TABLE IF NOT EXISTS `client_signup` (
+  `Name of Company` varchar(200) NOT NULL,
+  `E-Mail` varchar(200) NOT NULL,
+  `Password` varchar(200) NOT NULL,
+  `Description` varchar(200) NOT NULL,
+  `Job Roles` varchar(200) NOT NULL,
+  `Submit` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
